d62ee8fc66644e36d9914d95ada22fe8182765c9
mohamedali069 <mohamedali069@gmail.com>
2025-02-20 04:00:09 +0200

    third commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,33 +4,33 @@ import Title from './Title'
 const Contact = () => {
 
 
-    const [result, setResult] = React.useState("");
-  
-    const onSubmit = async (event) => {
-      event.preventDefault();
-      setResult("Sending....");
-      const formData = new FormData(event.target);
-  
-      formData.append("access_key", "9d60bd88-4319-48d7-b89b-cd4548993fa7");
-  
-      const response = await fetch("https://api.web3forms.com/submit", {
-        method: "POST",
-        body: formData
-      });
-  
-      const data = await response.json();
-  
-      if (data.success) {
-        setResult("Form Submitted Successfully");
-        event.target.reset();
-      } else {
-        console.log("Error", data);
-        setResult(data.message);
-      }
-    };
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
 
 
-
+  
 
   return (
     <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
@@ -53,7 +53,7 @@ const Contact = () => {
                 <textarea className='w-full px-4 py-3 h-48 border rounded border-gray-300 mt-2 resize-none' 
                 placeholder='Your Messege' name='messege' required/>
           </div>
-          <button className='px-12 py-2 mb-10 bg-blue-600 hover:bg-blue-500 text-white rounded cursor-pointer'>Send Message</button>
+          <button className='px-12 py-2 mb-10 bg-blue-600 hover:bg-blue-500 text-white rounded cursor-pointer'>{result ? result : "Send Message"}</button>
       </form>
     </div>
   )
